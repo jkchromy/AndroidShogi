b2f66f8d0898c3b23572fd58dd8a1a47231dd66b
jkchromy <jkchromy@gmail.com>
2022-02-22 17:25:02 -0700

    initial commit - deprecated JCenter() to mavenCentral()

---
+++ app/build/generated/source/buildConfig/debug/mobi/omegacentauri/shogi/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mobi.omegacentauri.shogi;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mobi.omegacentauri.shogi";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 366;
+  public static final String VERSION_NAME = "3.66";
+}
